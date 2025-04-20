e1b4723956161e12bd3eaa6ab690142b12d6868d
KimNganTP5813 <akinosora13@gmail.com>
2025-04-20 23:30:55 +0900

    Add files via upload

---
+++ ColorBox.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function ColorBox() {
+  return (
+    <>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          backgroundColor: "red",
+        }}
+      ></div>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          backgroundColor: "blue",
+        }}
+      ></div>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          backgroundColor: "green",
+        }}
+      ></div>
+    </>
+  );
+}
